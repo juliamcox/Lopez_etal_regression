79e55064722b05ef57a5b2ac53898729934b7b2f
juliamcox <89265451+juliamcox@users.noreply.github.com>
2025-02-08 13:54:58 -0600

    ...

--- plotting/plotBootCorr.m
+++ plotting/plotBootCorr.m
@@ -1,6 +1,7 @@
 function stats = plotBootCorr(realStats,shuffStats,params,eventNames,CIFlag,saveLoc,fname)
 
 al = .05./(numel(params.sessIDs)*numel(params.regions));
+plotColors = cat(1,[0 0 0;[34 194 227]./255]);
 
 %% Plot correlation coefficient for the full model 
 
@@ -8,8 +9,12 @@ for nr = 1:numel(params.regions)
     for ns = 1:numel(params.sessIDs)
         mu(nr,ns) = realStats{nr}{ns}.corr.full;
         clear thisShuff
+        try
         for nss = 1:params.numShuff
             thisShuff(nss) = shuffStats{nr}{ns}{nss}.corr.full;
+        end
+        catch
+            thisShuff = nan(1,params.numShuff);
         end
        mu(nr,ns) = median(thisShuff, 'omitmissing');
         if CIFlag
@@ -34,15 +39,16 @@ end
 
 f=figure('Units','inches','Position',[4.5729 2.8854 1.9427 1.6253]); hold on
 plot(x,y_hat,'k')
-p(1)=scatter(x,mu(1,:),10,'MarkerFaceColor','k','MarkerEdgeColor','none');
-if CIFlag
-    plot([x; x], [errNeg(1,:);errPos(1,:)],'k')
-    plot([x; x], [errNeg(2,:);errPos(2,:)],'k')
-else
-    errorbar(x,mu(1,:),errNeg(1,:),errPos(1,:),'LineStyle','none','CapSize',0,'Color','k')
-    errorbar(x,mu(2,:),errNeg(2,:),errPos(2,:),'LineStyle','none','CapSize',0,'Color','k')
+for nr = 1:numel(params.regions)
+    p(nr)=scatter(x,mu(nr,:),10,'MarkerFaceColor',plotColors(nr,:),'MarkerEdgeColor','none');
+    if CIFlag
+        for nr = 1:numel(params.regions)
+            plot([x; x], [errNeg(nr,:);errPos(nr,:)],'k')
+        end
+    else
+        errorbar(x,mu(nr,:),errNeg(nr,:),errPos(nr,:),'LineStyle','none','CapSize',0,'Color',plotColors(nr,:))
+    end
 end
-p(2)=scatter(x,mu(2,:),10,'MarkerEdgeColor','none','MarkerFaceColor',[34 194 227]./255);
 g=gca;
 g.YLim(1)=0;
 g.XLim = [.5 x(end)+.5];
@@ -62,11 +68,15 @@ for ne = 1:numel(eventNames)
         for ns = 1:numel(params.sessIDs)
             %mu(nr,ns)  = realStats{nr}{ns}.corr.full-eval(sprintf('realStats{nr}{ns}.corr.%s',eventNames{ne}));
             clear thisShuff
-            for nss = 1:params.numShuff
-                thisShuff_full(nss) = shuffStats{nr}{ns}{nss}.corr.full;
-                thisShuff_reduced(nss)  =  eval(sprintf('shuffStats{nr}{ns}{nss}.corr.%s',eventNames{ne}));
+            try
+                for nss = 1:params.numShuff
+                    thisShuff_full(nss) = shuffStats{nr}{ns}{nss}.corr.full;
+                    thisShuff_reduced(nss)  =  eval(sprintf('shuffStats{nr}{ns}{nss}.corr.%s',eventNames{ne}));
+                end
+                thisShuff = thisShuff_full-thisShuff_reduced;
+            catch
+                thisShuff = nan(1,params.numShuff);
             end
-            thisShuff = thisShuff_full-thisShuff_reduced;
             mu(nr,ns) = median(thisShuff,'omitmissing');
             shuff{nr,ns} = thisShuff;
             CI = prctile(thisShuff,[100*al/2,100*(1-al/2)]);
@@ -96,21 +106,17 @@ for ne = 1:numel(eventNames)
 
     f=figure('Units','inches','Position',[4.5729 2.8854 2.1357 1.6253]); hold on
     x = 1:numel(params.sessIDs);
+    for nr = 1:numel(params.regions)
     if CIFlag
-        plot([x+.1; x+.1], [errNeg(1,:);errPos(1,:)],'k')
-        plot([x-.1; x-.1], [errNeg(2,:);errPos(2,:)],'k')
-        y(1,:) = errPos(1,:)+.002;
-        y(2,:) = errPos(2,:)+.002;
+        plot([x+.1; x+.1], [errNeg(nr,:);errPos(nr,:)],'Color',plotColors(nr,:))
+        y(nr,:) = errPos(nr,:)+.002;
     else
-        errorbar(x+.1,mu(1,:),errNeg(1,:),errPos(1,:),'LineStyle','none','CapSize',0,'Color','k')
-        errorbar(x-.1,mu(2,:),errNeg(2,:),errPos(2,:),'LineStyle','none','CapSize',0,'Color','k')
-        y(1,:) = mu(1,:)+errPos(1,:)+.002;
-        y(2,:) = mu(2,:)+errPos(2,:)+.002;
+        errorbar(x+.1,mu(1,:),errNeg(nr,:),errPos(nr,:),'LineStyle','none','CapSize',0,'Color',plotColors(nr,:))
+        y(nr,:) = mu(nr,:)+errPos(nr,:)+.002;
     end
-    p(1)=scatter(x+.1,mu(1,:),10,'MarkerFaceColor','k','MarkerEdgeColor','none');
-    text(x(H(1,:))+.1,y(1,H(1,:)),'*','FontSize',10)
-    p(2)=scatter(x-.1,mu(2,:),10,'MarkerEdgeColor','none','MarkerFaceColor',[34 194 227]./255);
-    text(x(H(2,:))-.1,y(2,H(2,:)),'*','FontSize',10)
+    p(nr)=scatter(x+.1,mu(nr,:),10,'MarkerFaceColor',plotColors(nr,:),'MarkerEdgeColor','none');
+    text(x(H(nr,:))+.1,y(1,H(nr,:)),'*','FontSize',10)
+    end
     plot([.5 x(end)+.5],[0 0],':','Color',[0 0 0 .5])
     g=gca;
     g.XLim = [.5 x(end)+.5];
